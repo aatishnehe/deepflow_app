b37e692af4510a30c0aba02d03778d2c48c59b68
aatishnehe <nehe@str-ucture.com>
2023-06-28 14:24:32 +0200

    !

--- stream.py
+++ stream.py
@@ -622,7 +622,7 @@ def main():
     st.markdown("Germany")
     st.markdown("Email: [info@str-ucture.com](mailto:info@str-ucture.com)")
     st.markdown("Phone: [+49 (0)711 286937-0](tel:+49 (0)711 286937-0)")
-    st.markdown("<small>")
+    st.markdown("</small>")
 
 main()
 
